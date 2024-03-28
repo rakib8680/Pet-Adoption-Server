806d33a03674b1815c20b1802335a791ad65db00
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-28 16:11:29 +0600

    Add dependencies for CORS and Express types

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
+const app: Application = express();
 
+// middlewares
+app.use(cors());
+
+// application routes
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Pet Adoption 🐾",
+  });
+});
+
+export default app;
