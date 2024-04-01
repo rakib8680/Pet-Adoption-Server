55a24141d760d89061f3f49f883bb0bd9ef4ac8b
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-01 17:32:49 +0600

    Add validation middleware to create pet route

--- src/app/modules/pets/pet.routes.ts
+++ src/app/modules/pets/pet.routes.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import { PetControllers } from "./pet.controller";
 import auth from "../../middlewares/auth";
-
+import validateRequest from "../../middlewares/validateRequest";
+import { petValidations } from "./pet.validation";
 
 const router = express.Router();
 
-
-// routes 
-router.post("/pets", auth(), PetControllers.addPet);
+// routes
+router.post(
+  "/pets",
+  auth(),
+  validateRequest(petValidations.createPetValidationSchema),
+  PetControllers.addPet
+);
 
 router.get("/pets", PetControllers.getAllPets);
 
 router.put("/pets/:petId", auth(), PetControllers.updatePet);
-
-
-
 
 export const PetRoutes = router;
